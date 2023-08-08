8a3ab81843308af0833ce02354e58033a0300095
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-08-08 15:33:32 -0600

    Modelo for Control

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const { leerInput } = require('./helpers/inquirer.js');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer.js');
+const Busquedas = require('./models/busquedas.js');
 
 const main = async()=>{
-    const texto = await leerInput('Hola: ');
-    console.log( texto );
+    const busquedas = new Busquedas();
+    let opt='';
+
+    do {
+        //Imprimir menu
+        opt = await inquirerMenu();
+        switch (opt) {
+            case 1:
+                const lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad',)
+                console.log('Lat',)
+                console.log('Lng',)
+                console.log('Temperatura',)
+                console.log('Mínima',)
+                console.log('Máxima', )
+                break;
+            case 2:
+                    console.log('Presionaste 2 \n')
+                break;
+        }
+        if ( opt !==0) await pausa();
+    } while (opt !==0);
 }
 
 main();
---
+++ models/busquedas.js
@@ -0,0 +1,21 @@
+
+
+
+class Busquedas {
+    historial = ['Tegucipal','Madrid', 'San José'];
+
+    constructor (){
+        // TODO; leer DB si existe
+    }
+
+    async ciudad ( lugar= ''){
+        // Peticion http
+        HTMLFormControlsCollection.log(lugar);
+
+        return [];  //Retornar los lugares que coincidan. 
+    }
+
+
+}
+
+module.exports = Busquedas;
